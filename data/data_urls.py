4f37d3ca28241e626c1a5e9b596cc104a32699a1
Lipatnikova Anastasia <106307423+Lipatnikova@users.noreply.github.com>
2023-11-23 12:44:01 +0400

    Add urls (#2)

--- data/data_urls.py
+++ data/data_urls.py
@@ -2,3 +2,6 @@ class DataUrls:
     LOGIN_URL = 'https://www.saucedemo.com/v1/index.html'
     INVENTORY_URL = 'https://www.saucedemo.com/v1/inventory.html'
     CART_URL = 'https://www.saucedemo.com/v1/cart.html'
+    CHECKOUT_ONE_URL = 'https://www.saucedemo.com/v1/checkout-step-one.html'
+    CHECKOUT_TWO_URL = 'https://www.saucedemo.com/v1/checkout-step-two.html'
+    CHECKOUT_COMPLETE_URL = 'https://www.saucedemo.com/v1/checkout-complete.html'
